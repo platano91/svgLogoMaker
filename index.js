d26ef88b8ed4a1b187950e2d7ca9f1741f02b34a
Samuel <salvadomo@hotmail.com>
2024-01-09 13:11:50 -0700

    function to prompt

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const { Circle, Triangle, Square } = require('./lib/shapes');
+
+// Function to prompt user for logo creation
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'text',
+            message: 'Enter up to three characters for the logo:',
+            validate: input => input.length <= 3 ? true : 'Please enter no more than three characters.'
+        },
+        {
+            type: 'input',
+            name: 'textColor',
+            message: 'Enter the text color (keyword or hexadecimal):',
+            validate: input => /^#?([a-f0-9]{6}|[a-f0-9]{3})$/i.test(input) || /^[a-zA-Z]+$/i.test(input) ? true : 'Please enter a valid color (keyword or hex).'
+        },
+        {
+            type: 'list',
+            name: 'shape',
+            message: 'Choose a shape:',
+            choices: ['circle', 'triangle', 'square']
+        },
+        {
+            type: 'input',
+            name: 'shapeColor',
+            message: 'Enter the shape color (keyword or hexadecimal):',
+            validate: input => /^#?([a-f0-9]{6}|[a-f0-9]{3})$/i.test(input) || /^[a-zA-Z]+$/i.test(input) ? true : 'Please enter a valid color (keyword or hex).'
+        }
+    ]);
+}
+
